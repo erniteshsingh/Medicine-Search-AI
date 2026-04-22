e5b4ffb2d20b2896a3464caec1d0c570a615155f
niteshsingh <er.niteshkrsingh@gmail.com>
2026-04-22 14:50:54 +0530

    fix: standardized AppRoutes import and case sensitivity

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import AppRoutes from "./routes/Approutes.jsx";
-
+import AppRoutes from "./routes/AppRoutes.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
